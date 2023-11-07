c5a28bc5c3691914a4e103294bd22fe4a53d3454
Alvin Yusuf Riziq <alvinysfriz1@std.unissula.ac.id>
2023-11-07 15:18:21 +0700

    refactor <img> to <Image />

--- app/components/icons/Arrow.jsx
+++ app/components/icons/Arrow.jsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 const BackButton = () => {
   return (
     <div>
-      <img src="/arrow.png" alt="Back" width={30} height={30}/>
+      <Image src={'/arrow.png'} alt="Back" width={30} height={30} />
     </div>
   );
 };
