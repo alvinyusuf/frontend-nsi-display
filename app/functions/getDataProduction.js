4dd641b0abebb1cd0b3cacdfd936b568bb926361
Alvin Yusuf Riziq <alvinysfriz1@std.unissula.ac.id>
2023-11-13 09:12:21 +0700

    FIX: target from api

--- app/functions/getDataProduction.js
+++ app/functions/getDataProduction.js
@@ -13,22 +13,22 @@ export default async function getDataProduction() {
     console.error(error);
     return [
       {
-        PostDate: new Date(),
+        PostDate: '2023-11-13T00:34:06.956Z',
         LineType: 'CAM',
         RataRata: 0,
       },
       {
-        PostDate: new Date(),
+        PostDate: '2023-11-13T00:34:06.956Z',
         LineType: 'LINE1',
         RataRata: 0,
       },
       {
-        PostDate: new Date(),
+        PostDate: '2023-11-13T00:34:06.956Z',
         LineType: 'LINE2',
         RataRata: 0
       },
       {
-        PostDate: new Date(),
+        PostDate: '2023-11-13T00:34:06.956Z',
         LineType: 'LINE3',
         RataRata: 0,
       },
