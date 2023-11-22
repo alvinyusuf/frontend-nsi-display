e926a85f6942a75f159eb013265af6e6ff9927b1
Alvin Yusuf Riziq <alvinysfriz1@std.unissula.ac.id>
2023-11-22 15:11:22 +0700

    FIX: header dan nama title

--- app/components/header/mainHeader.jsx
+++ app/components/header/mainHeader.jsx
@@ -2,25 +2,17 @@
 
 import Arrow from "@/app/components/icons/Arrow"
 
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { usePathname, useRouter } from 'next/navigation';
 
 export default function MainHeader() {
+  const [dateState, setDateState] = useState(new Date());
+  useEffect(() => {
+    setInterval(() => setDateState(new Date()), 30000);
+  }, []);
+
   const pathname = usePathname()
-  const arrMonth = [
-    "Januari", "Februari", "Maret", "April",
-    "Mei", "Juni", "Juli", "Agustus", "September",
-    "Oktober", "November", "Desember",
-  ];
-
   const router = useRouter()
-
-  const now = new Date();
-  const day = now.getDate();
-  const month = now.getMonth();
-  const bulan = `${arrMonth[month]}`;
-  const year = now.getFullYear();
-  const dayDate = `${day} ${bulan} ${year}`;
 
   return (
     <div className="flex flex-row">
@@ -32,7 +24,13 @@ export default function MainHeader() {
       <div className="col w-full h-[150px] flex justify-center items-center">
         <div className="basis-10/12 text-center">
           <p className=" text-[64px]">DASHBORD CONTROL MANUFACTURING</p>
-          <p className=" text-[48px]">{dayDate}</p>
+          <p className=" text-[48px]">
+            {dateState.toLocaleDateString('en-GB', {
+              day: 'numeric',
+              month: 'long',
+              year: 'numeric',
+            })}
+          </p>
         </div>
       </div>
     </div>
